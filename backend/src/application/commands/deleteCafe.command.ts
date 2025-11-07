ddc241791f9b5649f0954866c4994d9975e149a9
MoeySeanJean <122952773+MoeySeanJean@users.noreply.github.com>
2025-11-07 22:56:56 +0800

    Revert to correct requirement

--- backend/src/application/commands/deleteCafe.command.ts
+++ backend/src/application/commands/deleteCafe.command.ts
@@ -1,9 +1,5 @@
 import prisma from '../../infrastructure/prismaClient';
 
 export const deleteCafe = async ({ id }: { id: string }) => {
-  await prisma.employee.updateMany({
-    where: { cafeId: id },
-    data: { cafeId: null, startDate: null },
-  })
   await prisma.cafe.delete({ where: { id } });;
 }
