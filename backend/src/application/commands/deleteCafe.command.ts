0a976b63d68e961d94d660d1d969295358b26172
MoeySeanJean <122952773+MoeySeanJean@users.noreply.github.com>
2025-11-07 22:49:57 +0800

    Fix delete cafe bug

--- backend/src/application/commands/deleteCafe.command.ts
+++ backend/src/application/commands/deleteCafe.command.ts
@@ -1,5 +1,9 @@
 import prisma from '../../infrastructure/prismaClient';
 
 export const deleteCafe = async ({ id }: { id: string }) => {
+  await prisma.employee.updateMany({
+    where: { cafeId: id },
+    data: { cafeId: null, startDate: null },
+  })
   await prisma.cafe.delete({ where: { id } });;
 }
